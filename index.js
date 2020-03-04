7639a102bffe6b623614207863cfc83035dccf69
Alejandro Rios <Alx.Rios91@gmail.com>
2020-03-03 23:04:28 -0600

    heroku test removed cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 //currently open to anyone, need to restrict it when done with front end
-app.use(cors());
+// app.use(cors());
 
 connectDB();
 
